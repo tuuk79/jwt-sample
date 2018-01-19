ded9b0028cf811d9f3616e5f62b852e701dd837c
Steve Yoo <s.yoo@avanade.com>
2018-01-19 12:49:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.get('/api', (req, res) => {
+	res.json({
+		text: 'my api!'
+	});
+});
+
+app.post('/api/login', (req, res) => {
+	const user = {
+		username: 'testname'
+	};
+	const token = jwt.sign(user, 'mysecretkey');
+	res.json({
+		token: token
+	});
+});
+
+app.get('/api/protected', ensureToken, (req, res) => {
+	jwt.verify(req.token, 'mysecretkey', (err, data) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			res.json({
+				text: 'this is protected',
+				data: data
+			});
+		}
+	});
+
+	res.json({
+		text: 'protect api!'
+	});
+});
+
+function ensureToken(req, res, next) {
+	const bearerHeader = req.headers['authorization'];
+	if (typeof bearerHeader!== 'undefined') {
+		const bearer = bearerHeader.split(' ');
+		const bearerToken = bearer[1];
+		req.token = bearerToken;
+		next();
+	} else {
+		res.sendStatus(403);
+	}
+}
+
+app.listen(3000, () => {
+	console.log('listening on port 3000!!!');
+});
